d0aad176201743c838a3367c43119daddedcc39f
Antony <antony.caron.burdiak@gmail.com>
2022-09-13 16:18:45 +0200

    "Clean" log events and availabilities

--- event.js
+++ event.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 let eventList = [];
 
 const Event = function(opening, recurring, startDate, endDate){
@@ -5,13 +7,27 @@ const Event = function(opening, recurring, startDate, endDate){
   this.recurring = recurring;
   this.startDate = startDate;
   this.endDate = endDate;
-
   eventList.push(this);
 };
 
-Event.prototype.availabilities = function(fromDate, toDate){
-  console.log(eventList)
-  return
+function date(recurring, date) {
+  let format = recurring ? 'dddd à HH:mm' : 'dddd Do MMMM YYYY à HH:mm';
+  return moment(date).locale('fr').format(format)
+}
+
+Event.prototype.availabilities = function(fromDate, toDate) {
+
+  eventList.forEach(event => {
+    let start = date(event.recurring, event.startDate);
+    let end = date(event.recurring, event.endDate);
+    const status = event.opening ? 'disponnible' : 'occupé';
+    const permanent = event.recurring ? 'tous les' : 'du';
+    console.log(`Un créneau est ${status} ${permanent} ${start} jusqu'au ${end}`);
+  });
+
+  let from = date(false, fromDate);
+  let to = date(false, toDate);
+  console.log(`Demande de dispo du ${from} au ${to}`);
 };
 
 export { Event }
